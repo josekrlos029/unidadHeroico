cc565afbb963f7b32aeefdb94ab6e7ec012893e7
josekrlos <josekrlos@192.168.88.46>
2014-12-05 07:02:31 -0500

    primera Prueba !

--- unidadHeroico/public_html/js/funciones.js
+++ unidadHeroico/public_html/js/funciones.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var lat, lng;
+var lat, lng, mapa;
 function cargarMapa(){
     $("#mapa").height($(document).height() / 2.5);
     navigator.geolocation.getCurrentPosition(function(position) {
